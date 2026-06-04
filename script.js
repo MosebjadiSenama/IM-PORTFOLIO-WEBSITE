9517c94e9ac384593ff530f9abfa31ca6e44b9d1
Mosebjadi-sudo <senamamosebjadi105@gmail.com>
2026-06-04 09:08:15 +0200

    IM PORTFOLIO WEBSITE

---
+++ script.js
@@ -0,0 +1,52 @@
+// HIRE ME BUTTON LINK
+
+document.querySelector(".hireme").addEventListener("click", function() 
+ {document.querySelector("#contact").scrollIntoView({
+    behavior:"smooth"
+ });
+     
+});
+
+//DISCOVER MY JOURNEY
+document.querySelector(".discover").addEventListener("click", function() {
+   window.location.href="about.html";
+    });
+
+//VIEW ALL PROJECTS 
+
+document.querySelector(".view-all-p").addEventListener("click", function() {
+   window.location.href="projects.html";
+    });
+
+
+//VIEW SASSA CASE STUDY
+
+document.querySelector(".view-casestudy").addEventListener("click", function() {
+   window.location.href="sassa.html";
+    });
+
+
+    //VIEW PRODEMIC CASE STUDY
+
+document.querySelector(".view-casestudy2").addEventListener("click", function() {
+   window.location.href="Prodemic.html";
+    });
+
+    //VIEW E-COMMERCE CASE STUDY
+
+document.querySelector(".view-casestudy3").addEventListener("click", function() {
+   window.location.href="E-commerce.html";
+    });
+
+
+
+    //BACK TO PROJECTS
+
+document.querySelector(".back-to-projects").addEventListener("click", function() {
+   window.location.href="projects";
+    });
+
+
+
+
+
